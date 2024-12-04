b7d932851c1913e66b97fd069b78c11c880d15b0
jianglibo <jianglibo@gmail.com>
2024-12-04 08:55:36 +0800

    more test.

--- gtest/boost_process_test.cpp
+++ gtest/boost_process_test.cpp
@@ -1,24 +1,86 @@
 #include <gtest/gtest.h>  // Add this line
 
 #include <boost/json.hpp>
+#include <boost/process.hpp>
 #include <boost/url.hpp>
 #include <cstddef>  // For std::ptrdiff_t
-#include <limits>   // For std::numeric_limits
+#include <iostream>
+#include <limits>  // For std::numeric_limits
 
-using namespace boost;
+#include "common_util.hpp"
 
+namespace pv2 = boost::process::v2;
 
+// https://www.boost.org/doc/libs/1_86_0/doc/html/boost_process/v2.html#boost_process.v2.introduction.pidfd_open
 
 TEST(BoostExampleProcessTest, starter) {
-  urls::url_view url(
-      "http://www.example.com:8080/path/to/"
-      "resource?query=string&query2=string2#fragment");
+  common::ThreadNotifier notifier(1000);
+  auto& iocManager = common::get_db_io_context_manager();
+  pv2::process proc(iocManager.ioc(), "/usr/bin/ls", {},
+                    pv2::process_stdio{{/*in to default*/}, stdout, stdout});
 
-  std::string_view sv =
-      url.path().substr(1);  // default return_string, it's temporary.
-  std::string s;
-  sv = url.path(urls::string_token::preserve_size(s))
-           .substr(1);  // default return_string, it's temporary.
-  ASSERT_EQ(sv, "path/to/resource");
-  ASSERT_EQ(s, "/path/to/resource");
+  std::cout << "pos 1" << std::endl;
+  proc.async_wait(
+      [&notifier](const boost::system::error_code& ec, int exit_code) {
+        if (ec) {
+          std::cerr << "Error: " << ec.message() << std::endl;
+        } else {
+          std::cout << "Exit code: " << exit_code << std::endl;
+        }
+        notifier.notify();
+      });
+  std::cout << "pos 2" << std::endl;
+  notifier.waitForNotification();
+  iocManager.stop();
+}
+
+TEST(BoostExampleProcessTest, sleep) {
+  common::ThreadNotifier notifier(1000);
+  auto& iocManager = common::get_db_io_context_manager();
+  pv2::process proc(iocManager.ioc(), "/usr/bin/sleep", {"10"});
+  std::cout << "pos 1" << std::endl;
+  proc.async_wait(
+      [&notifier](const boost::system::error_code& ec, int exit_code) {
+        if (ec) {
+          std::cerr << "Error: " << ec.message() << std::endl;
+        } else {
+          std::cout << "Exit code: " << exit_code << std::endl;
+        }
+        notifier.notify();
+      });
+  std::cout << "pos 2" << std::endl;
+  notifier.waitForNotification();
+  iocManager.stop();
+}
+
+TEST(BoostExampleProcessTest, sleepsync) {
+  common::ThreadNotifier notifier(1000);
+  auto& iocManager = common::get_db_io_context_manager();
+  pv2::process proc(iocManager.ioc(), "/usr/bin/sleep", {"10"});
+  std::cout << "pos 1" << std::endl;
+  proc.wait();
+  std::cout << "pos 2" << std::endl;
+  notifier.waitForNotification();
+  iocManager.stop();
+}
+TEST(BoostExampleProcessTest, rpipe) {
+  auto& iocManager = common::get_db_io_context_manager();
+  common::ThreadNotifier notifier(1000);
+  net::readable_pipe rp{iocManager.ioc()};
+  pv2::process proc(
+      iocManager.ioc(), "/usr/bin/g++", {"--version"},
+      pv2::process_stdio{{/* in to default */}, rp, {/* err to default */}});
+  boost::system::error_code ec;
+  std::string output;
+  std::string some_output;
+  while (size_t byte = rp.read_some(net::buffer(some_output), ec)) {
+    if (ec == net::error::eof) {
+      break;
+    }
+    output += some_output;
+  }
+  // std::this_thread::sleep_for(std::chrono::seconds(1));
+  proc.wait();
+  std::cout << "Output: " << output << std::endl;
+  notifier.waitForNotification();
 }
---
+++ lib/src/include/common_util.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+
+namespace net = boost::asio;  // from <boost/asio.hpp>
+
+namespace common {
+class ThreadNotifier {
+ public:
+  ThreadNotifier(uint64_t milliseconds = 0ull)
+      : notified_(false), milliseconds_(milliseconds) {}
+
+  void waitForNotification() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    // Wait until notified
+    if (milliseconds_ == 0) {
+      // In other words:
+      // It wakes up when the condition variable is notified.
+      // It continues execution only if the predicate evaluates to true.
+      // If the predicate returns false, it will keep waiting.
+      condition_.wait(
+          lock, [this] { return notified_; });  // predicate checks if notified
+    } else {
+      // Single Timeout Period: wait_for only waits for the specified
+      // duration once, measured from the moment the function is called.
+      // Notification Handling: If the condition variable is notified during the
+      // timeout: The predicate is evaluated. If the predicate returns true, the
+      // thread proceeds. If the predicate returns false, the thread continues
+      // waiting within the same timeout period. Timeout Expiry: If the
+      // specified duration expires, wait_for returns, regardless of the
+      // predicate's state.
+      condition_.wait_for(lock, std::chrono::milliseconds(milliseconds_),
+                          [this] { return notified_; });
+    }
+    notified_ = false;  // Reset the notification status
+    std::cout << "Thread notified!" << std::endl;
+  }
+
+  void notify() {
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      notified_ = true;  // Set the notification status
+    }
+    condition_.notify_one();  // Notify one waiting thread
+  }
+
+ private:
+  std::mutex mutex_;
+  std::condition_variable condition_;
+  bool notified_;
+  uint64_t milliseconds_;
+};
+
+class IoContextManager {
+ public:
+  IoContextManager(int threads_num = 0)
+#ifdef DEBUG_BUILD
+      : threads_num_(1),
+#else
+      : threads_num_(threads_num == 0 ? std::thread::hardware_concurrency()
+                                      : threads_num),
+#endif
+        ioc_(threads_num_),
+        work_guard(net::make_work_guard(ioc_)) {
+    threads_.reserve(threads_num_);
+    for (int i = 0; i < threads_num_; ++i) {
+      threads_.emplace_back([this] { ioc_.run(); });
+    }
+  }
+
+  boost::asio::io_context& ioc() { return ioc_; }
+
+  void stop() {
+    ioc_.stop();
+    for (auto& thread : threads_) {
+      if (thread.joinable()) {
+        thread.join();
+      }
+    }
+  }
+
+  ~IoContextManager() { stop(); }
+
+ private:
+  int threads_num_;
+  std::vector<std::thread> threads_;
+  boost::asio::io_context ioc_;
+  net::executor_work_guard<net::io_context::executor_type> work_guard;
+};
+
+inline IoContextManager& get_net_io_context_manager(int threads_num = 0) {
+  static IoContextManager manager(threads_num);
+  return manager;
+}
+
+inline IoContextManager& get_db_io_context_manager(int threads_num = 0) {
+  static IoContextManager manager(threads_num);
+  return manager;
+}
+
+}  // namespace common
